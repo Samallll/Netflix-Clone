629002589a9788e7a99b0e241080e3e03face7f0
Samallll <samalchokli@gmail.com>
2023-11-20 19:25:23 +0530

    basic netflix template created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './App.css'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
